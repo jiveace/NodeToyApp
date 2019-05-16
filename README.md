548dfc50d2266a93a7eb4ed5ac553cfb1b95a305
jiveace <straightedgescotsman@gmail.com>
2019-05-16 15:25:05 +0100

    Updating README and adding IP functionality to dance app

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-"# NodeToyApp" 
+Steps Required To Deploy This To Kubernetes
+1) Enable GKE
+2) git clone https://github.com/jiveace/NodeToyApp.git
+2a) Ensure everything works with 'node server.js'
+3) docker image build -t dance/dance-web:0.1 .
+   docker tag dance/dance-web:0.1 eu.gcr.io/<project-id>/dance
+   docker image push eu.gcr.io/<project-id>/dance:latest
+4) https://console.cloud.google.com/gcr/images/<project-id>?project=<project-id>
+5) Create FREE cluster
+6) Connect to cluster 
+   * gcloud container clusters get-credentials dance-cluster --zone us-central1-a --project <project-id> 
+7) Update image referred to in web-deploy.yml
+   kubectl apply -f web-deploy.yml
+   kubectl apply -f web-sl.yml
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const dances = require('./dances.json');
+var os = require('os');
+var ifaces = os.networkInterfaces();
 
 const app = express();
 
@@ -8,8 +10,29 @@ app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
+  let ip= "";
+  Object.keys(ifaces).forEach(function (ifname) {
+    var alias = 0;
+
+    ifaces[ifname].forEach(function (iface) {
+      if ('IPv4' !== iface.family || iface.internal !== false) {
+        // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+        return;
+      }
+
+      if (alias >= 1) {
+        // this single interface has multiple ipv4 addresses
+	ip+=(ifname + ':' + alias, iface.address);
+      } else {
+        // this interface has only one ipv4 adress
+        ip+=(ifname, iface.address);
+      }
+      ++alias;
+    });
+  });
+
   res.render('index', {
-    title: 'Lovely Dances',
+    title: 'Lovely Dances (' + ip + ')',
     dances: dances.profiles
   });
 });
